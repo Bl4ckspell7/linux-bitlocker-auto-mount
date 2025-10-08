37bfcec1ae3c677a14a1a833665d145da8417a87
Bl4ckspell <bl4ckspell@proton.me>
2025-10-08 11:00:09 +0200

    improve startup script - add blackbox-terminal support

--- mount-on-startup/bitlocker-startup.sh
+++ mount-on-startup/bitlocker-startup.sh
@@ -2,4 +2,12 @@
 
 SCRIPT_DIRECTORY="/path/to/script/directory" # e.g. /home/USER/linux-bitlocker-auto-mount
 
-gnome-terminal -- bash -c "source $SCRIPT_DIRECTORY/python3-venv/bin/activate && python $SCRIPT_DIRECTORY/bitlocker-unlock-mount.py; exec bash"
+CMD="source \"$SCRIPT_DIRECTORY/python3-venv/bin/activate\"
+python \"$SCRIPT_DIRECTORY/bitlocker-unlock-mount.py\"
+exec bash"
+
+# ────────────────────
+# Choose your terminal
+# ────────────────────
+CMD="$CMD" gnome-terminal -- bash -c 'eval "$CMD"'
+# CMD="$CMD" blackbox-terminal -e bash -c 'eval "$CMD"'
